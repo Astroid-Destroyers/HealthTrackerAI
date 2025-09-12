c3f32f79c7c252e4c850a5157c18057d3eb858f4
Enspiron <42487888+Enspiron@users.noreply.github.com>
2025-09-11 19:42:16 -0700

    banner

--- config/site.ts
+++ config/site.ts
@@ -63,7 +63,6 @@ export const siteConfig = {
     github: "https://github.com/Astroid-Destroyers/",
     twitter: "https://twitter.com/hero_ui",
     docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+
   },
 };
