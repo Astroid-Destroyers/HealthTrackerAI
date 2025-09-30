d230c8ea6d64a60c16b5ae27030b6ea9b97f2231
rpalmon <new.roeepalmon@gmail.com>
2025-09-30 13:12:24 -0700

    more testing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,10 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <meta name="google-adsense-account" content="ca-pub-9891270784697044"></meta>
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-9891270784697044"></meta>
+
+      </Head>
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
