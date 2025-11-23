d0baf363ac4e56ec0c6490ab1ab2d5d658fe7dc9
Emilia Mahmoodi <emilia.mahmoodi.945@my.csun.edu>
2025-11-22 22:28:43 -0800

    Create workout page

---
+++ pages/workouts/index.tsx
@@ -0,0 +1,10 @@
+export default function Workouts() {
+    return (
+        <div style={{ padding: "40px", color: "white" }}>
+            <h1 style={{ fontSize: "28px", fontWeight: "bold" }}>Workouts</h1>
+            <p style={{ marginTop: "10px", color: "#ccc" }}>
+                This is where your workout logs will go.
+            </p>
+        </div>
+    );
+}
