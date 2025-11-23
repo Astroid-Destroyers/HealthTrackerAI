72995d47868268e12635bbfea58a09413116a06a
Emilia Mahmoodi <emilia.mahmoodi.945@my.csun.edu>
2025-11-22 22:33:28 -0800

    Made goals page

---
+++ pages/goals/index.tsx
@@ -0,0 +1,10 @@
+export default function Goals() {
+    return (
+        <div style={{ padding: "40px", color: "white" }}>
+            <h1 style={{ fontSize: "28px", fontWeight: "bold" }}>Goals</h1>
+            <p style={{ marginTop: "10px", color: "#ccc" }}>
+                Set your fitness and nutrition goals here.
+            </p>
+        </div>
+    );
+}
