f8d1d2a9b11a05d0d0c98af8eb0632aed89396e4
Emilia Mahmoodi <emilia.mahmoodi.945@my.csun.edu>
2025-11-22 22:30:00 -0800

    Create nutrition page

---
+++ pages/nutrition/index.tsx
@@ -0,0 +1,10 @@
+export default function Nutrition() {
+    return (
+        <div style={{ padding: "40px", color: "white" }}>
+            <h1 style={{ fontSize: "28px", fontWeight: "bold" }}>Nutrition</h1>
+            <p style={{ marginTop: "10px", color: "#ccc" }}>
+                Track your meals and calories here.
+            </p>
+        </div>
+    );
+}
